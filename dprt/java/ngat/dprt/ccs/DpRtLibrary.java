152d6a300cc686702197343979d87fc2b6e32ed2
Chris Mottram <C.Mottram@ljmu.ac.uk>
2005-03-31 13:19:16 +0000

    Added lockReducedFilename and unlockReducedFilename methods. Called when reducing calibration and exposure frames, to create a lock file around files that are being reduced. This is so another process can be monitoring the directory and transfer completely reduced FITS images elsewhere.

--- dprt/java/ngat/dprt/ccs/DpRtLibrary.java
+++ dprt/java/ngat/dprt/ccs/DpRtLibrary.java
@@ -1,11 +1,12 @@
 // DpRtLibrary.java
-// $Header: /space/home/eng/cjm/cvs/dprt/java/ngat/dprt/ccs/DpRtLibrary.java,v 0.5 2004-03-31 08:45:30 cjm Exp $
+// $Header: /space/home/eng/cjm/cvs/dprt/java/ngat/dprt/ccs/DpRtLibrary.java,v 0.6 2005-03-31 13:19:16 cjm Exp $
 package ngat.dprt.ccs;
 
 import java.lang.*;
 import java.io.*;
 
 import ngat.dprt.*;
+import ngat.fits.*;
 import ngat.message.base.*;
 import ngat.message.INST_DP.*;
 import ngat.util.logging.*;
@@ -14,14 +15,22 @@ import ngat.util.logging.*;
  * This class supports a JNI interface to the Data Pipeline (Real Time) C library for real time
  * FITS file data reduction, for the CCD Control System (CCS).
  * @author Chris Mottram LJMU
- * @version $Revision: 0.5 $
+ * @version $Revision: 0.6 $
  */
 public class DpRtLibrary implements DpRtLibraryInterface
 {
 	/**
 	 * Revision Control System id string, showing the version of the Class.
 	 */
-	public final static String RCSID = new String("$Id: DpRtLibrary.java,v 0.5 2004-03-31 08:45:30 cjm Exp $");
+	public final static String RCSID = new String("$Id: DpRtLibrary.java,v 0.6 2005-03-31 13:19:16 cjm Exp $");
+	/**
+	 * Instance of FITS filename handling code, for lock file code.
+	 */
+	protected FitsFilename fitsFilename = null;
+	/**
+	 * The logger for this DpRtLibrary.
+	 */
+	protected Logger logger = null;
 
 // DpRt.h
 	/**
@@ -106,13 +115,18 @@ public class DpRtLibrary implements DpRtLibraryInterface
 	/**
 	 * Constructor. Calls the super class constructor, and initialiseLoggerReference to set
 	 * the C layer's reference to the logger (for logging to Java from C).
+	 * Fits filename instance is constructed.
 	 * @see #initialiseLoggerReference
+	 * @see #fitsFilename
+	 * @see #logger
 	 * @see ngat.util.logging.LogManager#getLogger
 	 */
 	public DpRtLibrary()
 	{
 		super();
 		initialiseLoggerReference(LogManager.getLogger(this));
+		logger = LogManager.getLogger(this);
+		fitsFilename = new FitsFilename();
 	}
 
 	/**
@@ -166,13 +180,19 @@ public class DpRtLibrary implements DpRtLibraryInterface
 	 * Routine to call to reduce a calibration FITS file. The filename is processed. The resultant data is passed
 	 * back to the Java layer in the calibrateReduceDone object, which is then sent over the network to
 	 * tell the client the process has been completed.
+	 * <p>This method now also calls lockReducedFilename and unlockReducedFilename. These create a lock
+	 * file based on the input filename, so that a script monitoring dprt output can tell when we have
+	 * finished with a reduced filename.
 	 * @param inputFilename The string representation of the filename.
 	 * @param calibrateReduceDone The calibrate reduce done object, 
 	 * 	that will be filled in with the processed filename and
 	 * 	useful data the data pipeline has extracted (peak and mean counts).
+	 * @see #lockReducedFilename
+	 * @see #unlockReducedFilename
 	 */
 	public void DpRtCalibrateReduce(String inputFilename,CALIBRATE_REDUCE_DONE calibrateReduceDone) 
 	{
+		lockReducedFilename(inputFilename);
 		// DpRt_Calibrate_Reduce is a Java Native routine that actually does the work.
 		// It will return TRUE even when the Data Pipeline failed - the calibrateReduceDone object
 		// contains the getSuccessful and getErrorNum routine to determine whether the 
@@ -187,19 +207,26 @@ public class DpRtLibrary implements DpRtLibraryInterface
 			calibrateReduceDone.setErrorString(this.getClass().getName()+
 				":DpRtCalibrateReduce:JNI interface failed.");
 		}
+		unlockReducedFilename(inputFilename);
 	}
 
 	/**
 	 * Routine to call to reduce an expose FITS file. The filename is processed. The resultant data is passed
 	 * back to the Java layer in the exposeReduceDone object, which is then sent over the network to
 	 * tell the client the process has been completed.
+	 * <p>This method now also calls lockReducedFilename and unlockReducedFilename. These create a lock
+	 * file based on the input filename, so that a script monitoring dprt output can tell when we have
+	 * finished with a reduced filename.
 	 * @param inputFilename The string representation of the filename.
 	 * @param exposeReduceDone The expose reduce done object, 
 	 * 	that will be filled in with the processed filename and
 	 * 	useful data the data pipeline has extracted (FWHM, Counts and location of brightest object etc).
+	 * @see #lockReducedFilename
+	 * @see #unlockReducedFilename
 	 */
 	public void DpRtExposeReduce(String inputFilename,EXPOSE_REDUCE_DONE exposeReduceDone)
 	{
+		lockReducedFilename(inputFilename);
 		// DpRt_Expose_Reduce is a Java Native routine that actually does the work.
 		// It will return TRUE even when the Data Pipeline failed - the exposeReduceDone object
 		// contains the getSuccessful and getErrorNum routine to determine whether the 
@@ -214,6 +241,7 @@ public class DpRtLibrary implements DpRtLibraryInterface
 			exposeReduceDone.setErrorString(this.getClass().getName()+
 				":DpRtExposeReduce:JNI interface failed.");
 		}
+		unlockReducedFilename(inputFilename);
 	}
 
 	/**
@@ -270,9 +298,131 @@ public class DpRtLibrary implements DpRtLibraryInterface
 	{
 		DpRt_Abort();
 	}
+
+	/**
+	 * Method to create a lock file based on the unreduced filename. Any present reduction flag
+	 * is changed to PIPELINE_PROCESSING_FLAG_REAL_TIME, and the '.fits' extension changed to .lock.
+	 * A blank filename is then generated.
+	 * @param unreducedFilename A string containing the unreduced FITS filename. The lock file produced
+	 *        should have the real time pipeline processing flag set, and have extension '.lock'.
+	 * @see #getLockFilename
+	 * @see #logger
+	 * @see ngat.fits.FitsFilename#PIPELINE_PROCESSING_FLAG_REAL_TIME
+	 * @see ngat.dprt.DpRtConstants.DPRT_LOG_LEVEL_LOCKFILES
+	 */
+	protected void lockReducedFilename(String unreducedFilename)
+	{
+		File file = null;
+		String lockFilename = null;
+		boolean retval;
+
+		logger.log(DpRtConstants.DPRT_LOG_LEVEL_LOCKFILES,this.getClass().getName()+
+				   ":lockReducedFilename: Generating lock filename based on "+unreducedFilename+".");
+		// get lock filename
+		lockFilename = getLockFilename(unreducedFilename);
+		if(lockFilename != null)
+		{
+			file = new File(lockFilename);
+			try
+			{
+				// NB see Java documentation, which states:
+				// this method should not be used for file-locking
+				retval = file.createNewFile();
+				logger.log(DpRtConstants.DPRT_LOG_LEVEL_LOCKFILES,this.getClass().getName()+
+					   ":lockReducedFilename: created lock : "+file+
+					   " : successfully created : "+retval+".");
+			}
+			catch(IOException e)
+			{
+				logger.log(DpRtConstants.DPRT_LOG_LEVEL_LOCKFILES,this.getClass().getName()+
+					   ":getLockFilename: Failed to create lock file "+file+" : "+e);
+				logger.dumpStack(DpRtConstants.DPRT_LOG_LEVEL_LOCKFILES,e);
+			}
+		}
+		logger.log(DpRtConstants.DPRT_LOG_LEVEL_LOCKFILES,this.getClass().getName()+
+				   ":lockReducedFilename: finished.");
+	}
+
+	/**
+	 * Method to delete a previously created lock file based on the unreduced filename. Any present reduction flag
+	 * is changed to PIPELINE_PROCESSING_FLAG_REAL_TIME, and the '.fits' extension changed to .lock.
+	 * A blank filename is then generated.
+	 * @param unreducedFilename A string containing the unreduced FITS filename. The lock file produced
+	 *        should have the real time pipeline processing flag set, and have extension '.lock'.
+	 * @see #getLockFilename
+	 * @see #logger
+	 * @see ngat.fits.FitsFilename#PIPELINE_PROCESSING_FLAG_REAL_TIME
+	 * @see ngat.dprt.DpRtConstants.DPRT_LOG_LEVEL_LOCKFILES
+	 */
+	protected void unlockReducedFilename(String unreducedFilename)
+	{
+		File file = null;
+		String lockFilename = null;
+		boolean retval;
+
+		logger.log(DpRtConstants.DPRT_LOG_LEVEL_LOCKFILES,this.getClass().getName()+
+				   ":unlockReducedFilename: Generating lock filename based on "+unreducedFilename+".");
+		// get lock filename
+		lockFilename = getLockFilename(unreducedFilename);
+		if(lockFilename != null)
+		{
+			file = new File(lockFilename);
+			retval = file.delete();
+			// Safety check in case of shutdown?
+			file.deleteOnExit();
+			logger.log(DpRtConstants.DPRT_LOG_LEVEL_LOCKFILES,this.getClass().getName()+
+				   ":unlockReducedFilename: lock : "+file+
+				   " : successfully deleted : "+retval+".");
+		}
+		logger.log(DpRtConstants.DPRT_LOG_LEVEL_LOCKFILES,this.getClass().getName()+
+				   ":unlockReducedFilename: finished.");
+	}
+
+	/**
+	 * Method to create a lock filename based on the unreduced filename. Any present reduction flag
+	 * is changed to PIPELINE_PROCESSING_FLAG_REAL_TIME, and the '.fits' extension changed to .lock.
+	 * @param unreducedFilename A string containing the unreduced FITS filename. 
+	 * @return A String containing the lock filename. This
+	 *        should have the real time pipeline processing flag set, and have extension '.lock'.
+	 *        This method can return 'null', if a parse error occurs.
+	 * @see #fitsFilename
+	 * @see #logger
+	 * @see ngat.fits.FitsFilename#PIPELINE_PROCESSING_FLAG_REAL_TIME
+	 * @see ngat.dprt.DpRtConstants.DPRT_LOG_LEVEL_LOCKFILES
+	 */
+	protected String getLockFilename(String unreducedFilename)
+	{
+		String lockFilename = null;
+
+		logger.log(DpRtConstants.DPRT_LOG_LEVEL_LOCKFILES,this.getClass().getName()+
+				   ":getLockFilename: Generating lock filename based on "+unreducedFilename+".");
+		try
+		{
+			fitsFilename.parse(unreducedFilename);
+			fitsFilename.setPipelineProcessing(FitsFilename.PIPELINE_PROCESSING_FLAG_REAL_TIME);
+			fitsFilename.setFileExtension("lock");
+		}
+		catch(Exception e)
+		{
+			logger.log(DpRtConstants.DPRT_LOG_LEVEL_LOCKFILES,this.getClass().getName()+
+				   ":getLockFilename: Failed to parse "+unreducedFilename+" : "+e);
+			logger.dumpStack(DpRtConstants.DPRT_LOG_LEVEL_LOCKFILES,e);
+			// NB lock filename not created!
+			return null;
+		}
+		lockFilename = fitsFilename.getFilename();
+		logger.log(DpRtConstants.DPRT_LOG_LEVEL_LOCKFILES,this.getClass().getName()+
+				   ":getLockFilename: lock filename : "+lockFilename+".");
+		return lockFilename;
+	}
 }
 //
 // $Log: not supported by cvs2svn $
+// Revision 0.5  2004/03/31 08:45:30  cjm
+// Repackaged into ngat.dprt.ccs.
+// DpRtLibrary now ccs specific. C library now called dprt_ccs.
+// Logger reference now generated from instance (i.e. ngat.dprt.ccs.DpRtLibrary.
+//
 // Revision 0.4  2004/01/30 17:01:00  cjm
 // Added logging support from C layer back up to Java.
 //
